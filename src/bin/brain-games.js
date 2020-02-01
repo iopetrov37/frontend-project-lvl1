4478ca02b086b780f391ebb0fa3f650c4edc28a8
Ilya Petrov <iopetrov37@gmail.com>
2020-02-01 21:50:02 +0300

    fix js files

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
-import { sayHiToUser } from '..';
+import userName from '..';
 
-console.log('Welcome to the Brain Games!');
-sayHiToUser();
+const name = userName();
+console.log('Welcome to the Brain Games\n');
+console.log(`May I have your name? ${name}`);
+console.log(`Hello, ${name}`);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import readlineSync from 'readline-sync';
 
-export const sayHiToUser = () => {
-  const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!`);
+const userName = () => {
+  const name = readlineSync.question('Your answer: ');
+  return name;
 };
+
+export default userName;
